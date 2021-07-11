20af3db1424399fa73623564d5643d441482c5f1
Vansh <vns.vansh123@gmail.com>
2021-07-11 18:54:55 +0530

    First Commit

---
+++ Pie_Chart/Pie_Chart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+print(plt.style.available)
+
+plt.style.use("fast")
+language=['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
+users=[59219, 55466, 47544, 36443, 35917]
+explode=[0,0,0,0.1,0]
+plt.pie(users,labels=language,explode=explode,startangle=90,autopct="%1.1f%%")
+
+plt.show()
